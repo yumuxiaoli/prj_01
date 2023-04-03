245cb0c28b1f84c8c5456be3886f027d2be4a3f9
yumu <2816083598@qq.com>
2023-04-03 19:42:08 +0800

    grom连接mysql数据库 2023/4/3

---
+++ models/UserBasic.go
@@ -0,0 +1,23 @@
+package models
+
+import "gorm.io/gorm"
+
+type UserBasic struct {
+	gorm.Model
+	Name          string
+	Password      string
+	Phone         string
+	Email         string
+	Identity      string // 唯一标识
+	ClientIP      string
+	ClientPort    string // 客户端口
+	LoginTime     uint64 // 登陆时间
+	HeartbeatTime uint64 // 心跳
+	LogoutTime    uint64 // 下线时间
+	IsLogout      bool   //	是否下线
+	DeviceInfo    string //设备信息
+}
+
+func (table *UserBasic) TableName() string {
+	return "user_basic"
+}
