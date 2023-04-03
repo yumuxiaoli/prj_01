98d4b5e88db20e51335bb8f5e2b25ebea4cc7cce
yumu <2816083598@qq.com>
2023-04-03 20:40:35 +0800

    2023/4/3

--- router/routerDao.go
+++ router/routerDao.go
@@ -1,11 +1,13 @@
 package router
 
 import (
+	"prj_01/service"
+
 	"github.com/gin-gonic/gin"
 )
 
 func Router() *gin.Engine {
 	r := gin.Default()
-	// r.GET("/", service.GetIndex)
+	r.GET("/", service.GetIndex)
 	return r
 }
