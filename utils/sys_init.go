78025f54a8908778280fb8d1a4a5e73c9d7b876f
yumu <2816083598@qq.com>
2023-04-04 18:47:05 +0800

    日志

--- utils/sys_init.go
+++ utils/sys_init.go
@@ -2,10 +2,14 @@ package utils
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"time"
 
 	"github.com/spf13/viper"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 var DB *gorm.DB
@@ -22,7 +26,18 @@ func InitConfig() {
 }
 
 func InitMySQL() {
-	DB, _ = gorm.Open(mysql.Open(viper.GetString("mysql.dns")), &gorm.Config{})
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags),
+		logger.Config{
+			SlowThreshold: time.Second,
+			LogLevel:      logger.Info,
+			Colorful:      true,
+		},
+	)
+
+	DB, _ = gorm.Open(mysql.Open(viper.GetString("mysql.dns")),
+		&gorm.Config{Logger: newLogger})
+	fmt.Println("MySQL inited...")
 	// if err != nil {
 	// 	panic("failed to connect database")
 	// }
